4fddd9b135cf8e9f697ed7b55dcbde0c94a27c57
ゆう <9cd01nqy@gmail.com>
2025-06-13 15:06:17 +0900

    Add files via upload

---
+++ JavaScript/index2.js
@@ -0,0 +1,29 @@
+// - フォームを入力し、送信に成功すると成功のメッセージが表示されます。
+
+// - 以下の場合、フォーム検証メッセージを受け取る
+
+// - 必須フィールドの入力漏れ
+
+// - メールアドレスが正しくフォーマットされていない
+
+// - キーボードだけでフォームを入力する
+
+// - 入力、エラーメッセージ、成功メッセージをスクリーンリーダーに表示する
+
+// - デバイスの画面サイズに応じて、インターフェイスの最適なレイアウトを表示する
+
+// - ページ上のすべてのインタラクティブ要素のホバーとフォーカスの状態を見る
+
+
+const fullName = document.getElementById(fullName);
+
+function checkText() {
+
+    const firstName = document.getElementById(firstName);
+    const lastName = document.getElementById(lastName);
+    const email = document.getElementById(email);
+
+    if(firstName == "" || firstName == null) {
+        
+    }
+}
